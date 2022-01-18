4c39ccef0abc3d279cf1ee422f5f55a6b5eff19e
jponharn <jukkrapong.ponharn@nectec.or.th>
2022-01-18 10:17:33 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# iaam-ng
+# iaam-ng-demo
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.3.
+
+## Development server
+
+Run `ng serve --ssl` for a dev server. Navigate to `https://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var iaam: any;
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    title = 'iaam-ng';
+    userProfile: any;
+    access_token: any;
+
+
+    ngOnInit() {
+        this.initIAAM();
+    }
+
+    initIAAM() {
+        try {
+            iaam.initLoginFlow({ issuer: "https://iaamclient.iaam.cloud", client_id: "6b749ebf-f9bd-4d0b-9ce2-88c6cc4a19fb" }).then(() => {
+                iaam.isLoggedIn((loggedIn: Boolean) => {
+                    if (loggedIn) {
+                        this.access_token = iaam.getAccessToken()
+                        iaam.getProfile()
+                            .then((profile: any) => {
+                                this.userProfile = profile
+                            })
+                    }
+                    else {
+                        iaam.login()
+                    }
+                })
+            })
+        } catch (err) {
+            // alert(err)
+        }
+    }
+    onLogout() {
+        iaam.logout();
+    }
+
+}
+
+
